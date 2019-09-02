9f86dee0a20cdb3dc89d3fc7fb2edade96e80e65
Brian C Sparks <brian.c.sparks@hp.com>
2019-09-02 08:10:45 -0700

    working

--- index.js
+++ index.js
@@ -2,64 +2,23 @@
 /**
  *
  */
-const sg                      = require('sgsg');
+const sg                      = require('sg-argv');
 const _                       = sg._;
-const runner                  = require('./lib/runner').main;
+
+const app                     = require('express')();
 const http                    = require('http');
-const urlLib                  = require('url');
 
 const ARGV                    = sg.ARGV();
-const argvGet                 = sg.argvGet;
-const argvExtract             = sg.argvExtract;
-const setOnn                  = sg.setOnn;
-const deref                   = sg.deref;
+const port                    = ARGV.port     | 3000;
 
-var lib = {};
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
 
-const main = function(callback) {
+const server = http.createServer(app);
 
+server.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
 
-  const hostname = '127.0.0.1';
-  const port = 6789;
-
-  const server = http.createServer((req, res) => {
-    // TODO: Maybe use getRawBody since we do not know it will be JSON
-    return sg.getBody(req, function() {
-
-      const url     = urlLib.parse(req.url, true);
-      const query   = url.query;
-
-      // The caller must format the input correctly:
-      //
-      //  all = {
-      //    command: 'string',
-      //    args:    ['the', 'args', 'to', 'pass']
-      //  }
-      //
-
-      const all   = _.extend({}, req.bodyJson || {}, query);
-
-      return runner(all, {}, function(err, data) {
-        res.setHeader('Content-Type', 'application/json');
-
-        if (err) {
-          res.statusCode = 400;
-          return res.end('{}');
-        }
-
-        res.statusCode = data.exitCode === 0 ? 200 : 400;
-        res.end(JSON.stringify(data));
-      });
-    });
-  });
-
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
-
-
-};
-
-
-main();
 

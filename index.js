60a6f060c3e9a1c9583b7a4393821bb5f50b1d79
Brian C Sparks <brian.c.sparks@hp.com>
2019-09-02 08:57:30 -0700

    all talking

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ const sg                      = require('sg-argv');
 const _                       = sg._;
 
 const app                     = require('express')();
-const http                    = require('http');
+const server                  = require('http').createServer(app);
+const io                      = require('socket.io')(server);
 
 const ARGV                    = sg.ARGV();
 const port                    = ARGV.port     | 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(`${__dirname}/index.html`);
 });
 
-const server = http.createServer(app);
+io.on('connection', (socket) => {
+  console.log(`a user connected`);
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit(`chat message`, msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server listening on ${port}`);
